5875d3968ee777fe2d752f5c1825d519ed60e085
Philip <philip.rodrigues.machado@gmail.com>
2023-09-12 12:18:18 -0300

    editado o arquivo conexao

--- conexao.js
+++ conexao.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '140515',
-        database: 'exerresolvido02'
+        host: 'XXX',
+        port: null,
+        user: 'XXX',
+        password: null,
+        database: 'XXX'
     },
     pool: { min: 0, max: 7 }
 });
